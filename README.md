14a03af70d68de9e4dce801402985ac4b38eec09
Storm OBryant <vmarie1997@gmail.com>
2022-06-09 04:27:23 -0500

    Labo 03 edits

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AboutMeProject
 Lab: 02 - About Me, with a guessing game
+Lab: 03 - Edited to add more content
--- script.js
+++ script.js
@@ -1,61 +1,84 @@
-"use strict"
-//greetings prompt at webpage load
-let userName = prompt("Hello, beautiful Human! What is your name?");
-alert("Nice to meet you, " + userName + ". Welcome to my quiz!");
+// // "use strict"
+// // //greetings prompt at webpage load
+// // let userName = prompt("Hello, beautiful Human! What is your name?");
+// // alert("Nice to meet you, " + userName + ". Welcome to my quiz!");
 
-//prompt on button click
-function quizTime(){
+// //prompt on button click
+// function quizTime(){
 
-	alert("Welcome to the Stormy Time Quiz. Please respond to each question with Y for yes and N for no");
+// 	alert("Welcome to the Stormy Time Quiz. Please respond to each question with Y for yes and N for no");
 
-	//question 1/5
-	let pronouns = prompt("Does Storm appreciate it if you refer to them with 'she' or 'her'?");
-	if (pronouns.toLowerCase() === "y"){
-		// console.log("That answer is definitely not correct.");
-		alert("That answer is definitely not correct.");
+// 	//question 1/5
+// 	let pronouns = prompt("Does Storm appreciate it if you refer to them with 'she' or 'her'?");
+// 	if (pronouns.toLowerCase() === "y"){
+// 		// console.log("That answer is definitely not correct.");
+// 		alert("That answer is definitely not correct.");
+// 	}else{
+// 		// console.log("You're correct! Storm uses they/them, not she/her");
+// 		alert("You're correct! Storm uses they/them, not she/her");
+// 	}
+
+// 	//question 2/5
+// 	let business = prompt("Is Storm a small business owner?");
+// 	if (business.toLowerCase() === "y"){
+// 		// console.log("You're correct! Storm sells their crocheted wares!");
+// 		alert("You're correct! Storm sells their crocheted wares!");
+// 	}else{
+// 		// console.log("I'm sorry, but that isn't correct");
+// 		alert("I'm sorry, but that isn't correct");
+// 	}
+
+// 	//question 3/5
+// 	let education = prompt("Did Storm ever study theatre?");
+// 	if (education.toLowerCase() === "y"){
+// 		// console.log("You're correct! Storm studied at the National Theatre Institute.");
+// 		alert("You're correct! Storm studied at the National Theatre Institute.");
+// 	}else{
+// 		// console.log("That answer is incorrect.");
+// 		alert("That answer is incorrect.");
+// 	}
+
+// 	//question 4/5
+// 	let jobs = prompt("Can Stormy keep a job?")
+// 	if (jobs.toLowerCase() === "y"){
+// 		// console.log("You didn't really read, did you?");
+// 		alert("You didn't really read, did you?");
+// 	}else{
+// 		// console.log("You're correct! Storm has a lot of job experience!");
+// 		alert("You're correct! Storm has a lot of job experience!");
+// 	}
+
+// 	//question 5/5
+// 	let goals = prompt("Did you have fun reading about Storm?");
+// 	if (goals.toLowerCase() === "y"){
+// 		// console.log("Aww, I love that!");
+// 		alert("Aww, I love that!");
+// 	}else{
+// 		// console.log("Damn. Welp, can't win them all");
+// 		alert("Damn. Welp, can't win them all");
+// 	}
+// 	alert("Thank you so much for taking my quiz, " + userName);
+// }
+
+//question 6/7
+// let howMany = parseInt(prompt("How many jobs do you think Storm has had so far? Type a number."));
+// for (let i = 0; i < 4; i++){
+// 	if (howMany === 12){
+// 		console.log("That's correct!")
+// 	}else{
+// 		while (howMany !== 12){
+// 			console.log("I'm sorry, try again!");
+// 		}
+// 	}console.log("The correct answer is 12!");
+//}
+
+let guess = parseInt(prompt("Guess how many jobs Storm has had so far"));
+for (let i = 0; i < 3; i++){
+	if(guess !== 12){
+		alert("Nope! Try again");
+		prompt("Guess how many jobs Storm has had so far");
 	}else{
-		// console.log("You're correct! Storm uses they/them, not she/her");
-		alert("You're correct! Storm uses they/them, not she/her");
+		alert("That's correct! You get a point.");
+		break;
 	}
-
-	//question 2/5
-	let business = prompt("Is Storm a small business owner?");
-	if (business.toLowerCase() === "y"){
-		// console.log("You're correct! Storm sells their crocheted wares!");
-		alert("You're correct! Storm sells their crocheted wares!");
-	}else{
-		// console.log("I'm sorry, but that isn't correct");
-		alert("I'm sorry, but that isn't correct");
-	}
-
-	//question 3/5
-	let education = prompt("Did Storm ever study theatre?");
-	if (education.toLowerCase() === "y"){
-		// console.log("You're correct! Storm studied at the National Theatre Institute.");
-		alert("You're correct! Storm studied at the National Theatre Institute.");
-	}else{
-		// console.log("That answer is incorrect.");
-		alert("That answer is incorrect.");
-	}
-
-	//question 4/5
-	let jobs = prompt("Can Stormy keep a job?")
-	if (jobs.toLowerCase() === "y"){
-		// console.log("You didn't really read, did you?");
-		alert("You didn't really read, did you?");
-	}else{
-		// console.log("You're correct! Storm has a lot of job experience!");
-		alert("You're correct! Storm has a lot of job experience!");
-	}
-
-	//question 5/5
-	let goals = prompt("Did you have fun reading about Storm?");
-	if (goals.toLowerCase() === "y"){
-		// console.log("Aww, I love that!");
-		alert("Aww, I love that!");
-	}else{
-		// console.log("Damn. Welp, can't win them all");
-		alert("Damn. Welp, can't win them all");
-	}
-	alert("Thank you so much for taking my quiz, " + userName);
 }
